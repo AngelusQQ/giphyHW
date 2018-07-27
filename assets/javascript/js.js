25dfebffb650fe574de86697f5d397e1a710bf05
William <william.ma9215@gmail.com>
2018-07-27 16:40:08 -0400

    Working Version

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,53 +1,45 @@
-var searchOn = false;
-var searchCounter = 0;
-
 $(document).ready (function() {
 
   function getData(search) {
 
-    var gif = search;
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=E6dCnW33DP2LRdQtDPL320f0vdv8VOBA&rating=g";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=E6dCnW33DP2LRdQtDPL320f0vdv8VOBA&rating=g";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(JSON.stringify(response));
+      for(var i = 0; i < 10; i++) {
+        var temp = $('<img>');
+        temp.attr("src", response.data[i].images.original_still.url);
+        temp.attr("class", "GIF");
+        $('#results').append(temp);
+        // console.log(response.data[0].images.original_still.url);
+        // console.log(response.data[0].images.original.url);
+      }
     });
-
   }
 
-  $('#submit').on("click", function() {
-    console.log($('#giphy').val().trim());
+  $('#submit').on("click", function(event) {
+    event.preventDefault();
+    var temp = $('<button></button>');
+    temp.text($('#search').val());
+    temp.addClass("gifbutton");
+    temp.css("width", "auto");
+    temp.css("height", "auto");
+    $('#buttons').append(temp);
+    $('#search').val("");
   });
 
-  $('#search').on("click", function() {
-    searchOn = true;
+  $(document).on("click", ".gifbutton", function() {
+    $('#results').empty();
+    getData(this.innerHTML);
   });
 
-  $('body').on("click", function() {
-    console.log(this);
-    searchOn = false;
-  });
-
-
-
-  $(document).on("keypress", function(event) {
-    var temp = String.fromCharCode(event.which);
-    if(searchCounter < 25 && event.which !== 13) {
-      $('#search').append(temp);
-      searchCounter += 1;
-    }
-  });
-
-  $(document).on("keydown", function(event) {
-    var temp = $('#search').text();
-    if(event.which === 8 && searchCounter > 0) {
-      $('#search').text(temp.substring(0, temp.length-1));
-      searchCounter -= 1;
-    } else if (event.which === 13 && searchCounter > 0) {
-      getData($('#search').text());
-      $('#search').text("");
+  $(document).on("click", ".GIF", function() {
+    if(this.getAttribute("src").includes("_s")) {
+      this.setAttribute("src", this.getAttribute("src").replace("_s.gif", ".gif"));
+    } else {
+      this.setAttribute("src", this.getAttribute("src").replace(".gif", "_s.gif"));
     }
   });
 
